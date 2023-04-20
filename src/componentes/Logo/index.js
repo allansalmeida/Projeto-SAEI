232ba7a9a6ae6309ad5173ea2fcb2a554168b7ed
Allan <allansalmeida93@gmail.com>
2023-04-20 06:13:09 -0300

    atualizado front

---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,21 @@
+import logo from '../../imagens/logo.svg'
+import './estilo.css'
+import styled from 'styled-components';
+
+
+function Logo(){
+    return (
+    <nav>
+        <img src={logo} alt='logo' className='logo-img'></img>
+        <a className="logo"> SAEI </a>
+        <ul className="nav-list">
+        <li><a href="inicio.html">Inicio</a></li>
+        <li><a href="notas.html">Notas</a></li>
+        <li><a href="ocorrencias.html">Ocorrências</a></li>
+        <li><a href="atividades.html">Atividades</a></li>
+    </ul>
+</nav>
+     )
+}
+
+export default Logo;
